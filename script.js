2bdf4cfe8376bf9d31da2598cb4077b4755eb2cf
Austin Fletcher <49542016+astn7278@users.noreply.github.com>
2024-04-17 22:34:54 -0700

    Test constructor workflow, add js comments

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const myLibrary = [];
 
-document.getElementById("newBookForm").addEventListener("submit", function(event) {
+//listen for form submit, assigns user inputs to variables, creates object using Book constructor
+document.getElementById("newBookForm").addEventListener("submit", function(event) { 
     event.preventDefault();
 let author = document.getElementById("authorName").value;
 let title = document.getElementById("bookTitle").value;
 let pages = document.getElementById("pageCount").value;
 let status = document.getElementById("status").checked;
+
 let userBook = new Book(author, title, pages, status);
 
 console.log(userBook)
 })
 
-
+//Object constructor takes variables, creates object, then pushes to array
 function Book(author, title, pages, status) {
   this.author = author;
   this.title = title;
@@ -21,6 +23,4 @@ function Book(author, title, pages, status) {
   myLibrary.push(this)
 }
 
-function addBookToLibrary() {
-    
-}
+
