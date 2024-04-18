94e64ba92d0ef1f6adfeb69a741452c23104d31e
Austin Fletcher <49542016+astn7278@users.noreply.github.com>
2024-04-17 22:09:45 -0700

    Assign user input to variables

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const myLibrary = [];
 
+document.getElementById("newBookForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+let author = document.getElementById("authorName").value;
+let title = document.getElementById("bookTitle").value;
+let pages = document.getElementById("pageCount").value;
+let status = document.getElementById("status").checked;
+let userBook = new Book(author, title, pages, status);
+
+console.log(userBook)
+})
+
+
 function Book(author, title, pages, status) {
   this.author = author;
   this.title = title;
