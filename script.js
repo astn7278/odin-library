d94398380a947f9156fb862f1aaa8f8df4829e2e
Austin Fletcher <49542016+astn7278@users.noreply.github.com>
2024-04-18 17:01:31 -0700

    Add logic for closing modal

--- script.js
+++ script.js
@@ -1,26 +1,48 @@
 const myLibrary = [];
 
 //listen for form submit, assigns user inputs to variables, creates object using Book constructor
-document.getElementById("newBookForm").addEventListener("submit", function(event) { 
-    event.preventDefault();
-let author = document.getElementById("authorName").value;
-let title = document.getElementById("bookTitle").value;
-let pages = document.getElementById("pageCount").value;
-let status = document.getElementById("status").checked;
+document
+    .getElementById("newBookForm")
+    .addEventListener("submit", function (event) {
+        event.preventDefault();
+        let author = document.getElementById("authorName").value;
+        let title = document.getElementById("bookTitle").value;
+        let pages = document.getElementById("pageCount").value;
+        let status = document.getElementById("status").checked;
 
-let userBook = new Book(author, title, pages, status);
+        let userBook = new Book(author, title, pages, status);
 
-console.log(userBook)
-})
+        console.log(userBook);
+        this.reset();
+    });
 
 //Object constructor takes variables, creates object, then pushes to array
 function Book(author, title, pages, status) {
-  this.author = author;
-  this.title = title;
-  this.pages = pages;
-  this.status = status;
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.status = status;
 
-  myLibrary.push(this)
+    myLibrary.push(this);
 }
 
+//Opens "new book" modal with button press
+document.getElementById("openModalBtn").addEventListener("click", function () {
+    document.getElementById("newBookModal").classList.add("show");
+});
 
+//Closes "new book" modal with cancel button
+document.getElementById("cancelButton").addEventListener("click", function () {
+    document.getElementById("newBookModal").classList.remove("show");
+});
+
+//Closes "new book" modal by clicking outside modal
+document.addEventListener("click", function (event) {
+    let modal = document.getElementById("newBookModal");
+    if (
+        !modal.contains(event.target) &&
+        !document.getElementById("openModalBtn").contains(event.target)
+    ) {
+        modal.classList.remove("show");
+    }
+});
